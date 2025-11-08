7b546a7e5b99791f8a293628673b0ab9e5def63d
TomCanTech <tomcantech@gmail.com>
2025-11-08 13:31:15 +0000

    Added amplitude shift to wave

--- assets/UI/Signal/wave.gd
+++ assets/UI/Signal/wave.gd
@@ -1,23 +1,30 @@
 extends Node2D
 
 const FREQ_INCREMENT: float = 0.6
-@onready var sine_wave_mat = $ColorRect.material
+@onready var rect_mat: ShaderMaterial = $ColorRect.material
+@onready var rect_init_size: Vector2 = $ColorRect.size
+@onready var size: Vector2 = $ColorRect.size
 
-var freq: float
-var time_scale: float
-var size: float
+@onready var freq: float = rect_mat.get_shader_parameter('freq')
+@onready var time_scale: float = rect_mat.get_shader_parameter('time_scale')
+@onready var thickness: float = rect_mat.get_shader_parameter('size')
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	freq = sine_wave_mat.get_shader_parameter('freq')
-	time_scale = sine_wave_mat.get_shader_parameter('time_scale')
-	size = sine_wave_mat.get_shader_parameter('size')
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_Q):
-		sine_wave_mat.set_shader_parameter('freq', freq - delta * FREQ_INCREMENT)
-		freq = sine_wave_mat.get_shader_parameter('freq')
+		change_amp(-50.0 * delta);
 	if Input.is_key_pressed(KEY_E):
-		sine_wave_mat.set_shader_parameter('freq', freq + delta * FREQ_INCREMENT)
-		freq = sine_wave_mat.get_shader_parameter('freq')
+		change_amp(50.0 * delta)
+
+func change_freq(freq_diff: float):
+	var freq_change: float = freq_diff + freq
+	clamp(freq_change, 1.0, 12.0)
+	rect_mat.set_shader_parameter('freq', freq + freq_diff)
+	freq = rect_mat.get_shader_parameter('freq')
+
+func change_amp(amp_diff: float):
+	var size_change: float = amp_diff + size.y
+	size_change = clampf(size_change, 0.00001, rect_init_size.y)
+	$ColorRect.set_size(Vector2(size.x, size_change))
+	
+	$ColorRect.position.y = ($ColorRect.position.y * $ColorRect.get_size().y) / size.y
+	size = $ColorRect.get_size()
